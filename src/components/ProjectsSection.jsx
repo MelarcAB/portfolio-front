4b8e0c2c10950399d05abd39f942d53b059fdb24
MelarcAB <melarc.ab@gmail.com>
2023-08-09 20:47:15 +0200

    tests vista projects

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -1,30 +1,41 @@
+import { motion } from "framer-motion"
+
 function ProjectsSection({ projects }) {
     return (
-        <section className="py-8 bg-gray-800 rounded-xl shadow-lg p-6 mt-8">
-            <h2 className="text-2xl font-bold mb-8 text-white">Projects</h2>
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <section className="py-8 mt-8 dark:bg-gray-800">
+            <h2 className="text-2xl font-bold mb-8 text-center text-black dark:text-white">Projects</h2>
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
                 {projects.map(project => (
-                    <div
+                    <motion.div
                         key={project.slug}
-                        className="border border-gray-700 rounded-lg p-4 transition transform hover:shadow-xl hover:scale-105">
+                        whileHover={{ scale: 1.05, y: -10 }}
+                        whileTap={{ scale: 0.95 }}
+                        className="flex space-x-4 bg-white bg-opacity-10 dark:bg-opacity-10 backdrop-blur-md dark:bg-gray-700 rounded-xl p-4">
 
-                        <img
-                            src={project.image}
-                            alt={project.name}
-                            className="mb-4 rounded w-full h-48 object-cover shadow-md hover:shadow-lg"
-                        />
+                        {/* Project Image */}
+                        <div className="w-24 h-24 overflow-hidden rounded-full shadow-lg">
+                            <img
+                                src={project.image}
+                                alt={project.name}
+                                className="object-cover w-full h-full"
+                            />
+                        </div>
 
-                        <h3 className="text-lg font-bold text-white mb-2">{project.name}</h3>
-                        <p className="text-gray-400 mb-3">{project.description}</p>
-                        <a
-                            href={project.demo_url}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-blue-400 hover:text-blue-500 hover:underline transition"
-                        >
-                            View Demo
-                        </a>
-                    </div>
+                        {/* Project Info */}
+                        <div>
+                            <h3 className="text-xl font-bold text-black dark:text-white">{project.name}</h3>
+                            <p className="text-gray-500 dark:text-gray-300 my-2">{project.description}</p>
+                            <a
+                                href={project.demo_url}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="text-blue-400 dark:text-blue-300 hover:text-blue-500 hover:underline"
+                            >
+                                View Demo
+                            </a>
+                        </div>
+                    </motion.div>
                 ))}
             </div>
         </section>
