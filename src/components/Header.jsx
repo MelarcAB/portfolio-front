535274b69303ae4e53c1f07adbaaa734766f86c0
MelarcAB <melarc.ab@gmail.com>
2023-08-12 01:42:09 +0200

    añado iconos a github y linkedin

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { motion } from 'framer-motion';
 import { MapPinIcon } from '@heroicons/react/24/solid';
 import { GithubIcon, LinkedinIcon } from './Icons/CustomIcons';
+
 function CircleHeader({ portfolio }) {
     if (!portfolio) return null;
 
@@ -9,10 +10,11 @@ function CircleHeader({ portfolio }) {
             initial={{ scale: 0.95, opacity: 0 }}
             animate={{ scale: 1, opacity: 1 }}
             transition={{ delay: 0.5, duration: 0.7 }}
-            className="p-8 md:p-16 relative flex flex-col items-center bg-white dark:bg-gray-800 border rounded-3xl shadow-xl mb-8"
+            className="p-8 md:p-16 relative flex flex-col items-center mb-8"
         >
 
-            <div className="w-60 h-60 overflow-hidden rounded-full border-4 border-indigo-500 mb-4 shadow-lg">
+            {/* Avatar Image */}
+            <div className="w-60 h-60 overflow-hidden rounded-full border-4 border-indigo-500 mb-4">
                 <motion.img
                     src={portfolio.image}
                     alt={portfolio.name}
@@ -20,42 +22,51 @@ function CircleHeader({ portfolio }) {
                 />
             </div>
 
-            <div className="text-center mb-5">
-                <motion.h1
-                    initial={{ y: -20, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{ delay: 0.3, duration: 0.5 }}
-                    className="text-5xl md:text-6xl font-extrabold text-gray-800 dark:text-white mb-2"
+            {/* User Info */}
+            <motion.h1
+                initial={{ y: -20, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ delay: 0.3, duration: 0.5 }}
+                className="text-5xl md:text-6xl font-extrabold text-gray-800 dark:text-white mb-2"
+            >
+                {portfolio.name}
+            </motion.h1>
+
+            <motion.p
+                initial={{ y: -10, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
+                transition={{ delay: 0.5, duration: 0.5 }}
+                className="text-xl text-indigo-500 dark:text-indigo-400 mb-4"
+            >
+                {portfolio.job_title} en
+                <a href={portfolio.current_company_website} target="_blank" rel="noopener noreferrer" className="underline hover:text-indigo-600">
+                    {portfolio.current_company}
+                </a>
+            </motion.p>
+
+            <p className="text-gray-600 dark:text-gray-300 mb-4">{portfolio.description}</p>
+
+            {/* GitHub and LinkedIn as Labels */}
+            <div className="flex space-x-3 justify-center">
+                <motion.a
+                    href={portfolio.git_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center space-x-2 inline-block px-3 py-1 text-sm font-bold text-black bg-gray-200 rounded hover:bg-gray-300 transition-colors"
                 >
-                    {portfolio.name}
-                </motion.h1>
+                    <GithubIcon className="w-4 h-4" />
+                    <span>GitHub</span>
+                </motion.a>
 
-                <motion.p
-                    initial={{ y: -10, opacity: 0 }}
-                    animate={{ y: 0, opacity: 1 }}
-                    transition={{ delay: 0.5, duration: 0.5 }}
-                    className="text-xl text-indigo-500 dark:text-indigo-400"
+                <motion.a
+                    href={portfolio.linkedin_url}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="flex items-center space-x-2 inline-block px-3 py-1 text-sm font-bold text-black bg-gray-200 rounded hover:bg-gray-300 transition-colors"
                 >
-                    {portfolio.job_title} en
-                    <a href={portfolio.current_company_website} target="_blank" rel="noopener noreferrer" className="underline hover:text-indigo-600">
-                        {portfolio.current_company}
-                    </a>
-                </motion.p>
-            </div>
-
-            <div className="text-center">
-                <p className="text-gray-600 dark:text-gray-300 mb-5">{portfolio.description}</p>
-
-                <div className="flex space-x-5 justify-center mb-4">
-                    <a href={portfolio.git_url} target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 hover:text-gray-800 dark:hover:text-gray-200 transition-colors">
-                        <GithubIcon className="h-6 w-6" />
-                        <span>GitHub</span>
-                    </a>
-                    <a href={portfolio.linkedin_url} target="_blank" rel="noopener noreferrer" className="flex items-center space-x-2 hover:text-gray-800 dark:hover:text-gray-200 transition-colors">
-                        <LinkedinIcon className="h-6 w-6" />
-                        <span>LinkedIn</span>
-                    </a>
-                </div>
+                    <LinkedinIcon className="w-4 h-4" />
+                    <span>LinkedIn</span>
+                </motion.a>
             </div>
         </motion.div>
     );
